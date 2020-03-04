8e19cda10c84c4216da31e591e79bef3ba6dfe20
Ahmed <ahmedabdo6868@gmail.com>
2020-03-04 12:20:55 +0200

    Trying http Call

--- js/main.js
+++ js/main.js
@@ -101,7 +101,7 @@ function getVariations() {
 
     $.ajax({
         type : "POST",
-        url : "https://tiles-app.westeurope.azurecontainer.io:3000/files/",
+        url : "http://tiles-app.westeurope.azurecontainer.io:3000/files/",
         // dataType : "json",
         contentType: "application/json; charset=utf-8",
         data : JSON.stringify(data),
@@ -109,7 +109,7 @@ function getVariations() {
             
             $('#mainViewImg').empty();
             $('#mainViewImg').append(`
-            <img class="img-responsive" src="https://tiles-app.westeurope.azurecontainer.io:3000/${result.message}" />
+            <img class="img-responsive" src="http://tiles-app.westeurope.azurecontainer.io:3000/${result.message}" />
             `);
 
         },
